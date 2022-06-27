ebaefe98c7d20ca6598dec13c8bdcb383f3c3a3e
Josue Halliwel <josuehalliwel@Josues-MacBook-Air.local>
2022-06-27 18:56:39 -0400

    update api and front end

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 document.querySelector('button').addEventListener('click', getTarot)
 function getTarot(){
-fetch("https://rws-cards-api.herokuapp.com/api/v1/cards/random")
+    fetch("https://tarot-api-proj.herokuapp.com/api/random")
     .then(res => res.json())
     .then(data => {
-        console.log(data.cards[0])
-        document.querySelector('h2').innerText = data.cards[0].desc
+        console.log(data.meaning)
+        document.querySelector('h2').innerHTML = data.meaning
 
     })
     .catch(err => {
